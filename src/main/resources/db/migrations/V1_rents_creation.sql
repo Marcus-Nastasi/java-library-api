62bf109090c9f9e23a4e4099aa64913d384be5ae
rolemberg <vinnie.nsts@gmail.com>
2024-06-10 02:00:18 -0300

    refactored rents table

--- src/main/resources/db/migrations/V1_rents_creation.sql
+++ src/main/resources/db/migrations/V1_rents_creation.sql
@@ -3,8 +3,10 @@ CREATE TABLE rents(
     id BIGINT NOT NULL PRIMARY KEY,
     emitDate DATE NOT NULL,
     returnDate DATE NOT NULL,
+    librarianId VARCHAR(255) NOT NULL,
     memberId BIGINT NOT NULL,
-    FOREIGN KEY (memberId) REFERENCES members(id)
+    FOREIGN KEY (memberId) REFERENCES members(id),
+    FOREIGN KEY (librarianId) REFERENCES librarian(id)
 );
 
 
