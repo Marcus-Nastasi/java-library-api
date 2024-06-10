013e3c37ceb9acb8166a74d720b174cb9aac1526
rolemberg <vinnie.nsts@gmail.com>
2024-06-10 01:55:02 -0300

    created members table

---
+++ src/main/resources/db/migrations/V1_members_creation.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE members(
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    cpf VARCHAR(255) NOT NULL UNIQUE,
+    type VARCHAR(255) NOT NULL,
+    dateOfMembership DATE NOT NULL
+    booksIssued INT NOT NULL,
+    booksLimit INT NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+);
+
+
+
+
+
