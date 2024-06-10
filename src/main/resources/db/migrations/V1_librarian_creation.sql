00ec3de4d231b47f5d9fd55970bd1241d6ca7108
rolemberg <vinnie.nsts@gmail.com>
2024-06-10 01:19:38 -0300

    created librarians table

---
+++ src/main/resources/db/migrations/V1_librarian_creation.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE librarian(
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    cpf VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+
+
