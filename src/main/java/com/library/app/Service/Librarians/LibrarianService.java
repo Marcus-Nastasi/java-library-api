5eefe62ff6495a7f5dc6998e1be6192a104f95a1
rolemberg <vinnie.nsts@gmail.com>
2024-06-11 23:58:30 -0300

    init librarian service class

--- src/main/java/com/library/app/Service/Librarians/LibrarianService.java
+++ src/main/java/com/library/app/Service/Librarians/LibrarianService.java
@@ -1,9 +1,23 @@
 package com.library.app.Service.Librarians;
 
+import com.library.app.Repository.Books.BooksRepo;
+import com.library.app.Repository.Librarian.LibrarianRepo;
+import com.library.app.Repository.Members.MembersRepo;
+import com.library.app.Repository.Rents.RentsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LibrarianService {
+
+    @Autowired
+    private LibrarianRepo librarianRepo;
+    @Autowired
+    private RentsRepo rentsRepo;
+    @Autowired
+    private BooksRepo booksRepo;
+    @Autowired
+    private MembersRepo membersRepo;
 
     // to-do: business rules implementation.
 
